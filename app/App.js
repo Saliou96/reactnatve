d265773b6f1af313bdae201e112d6ea05590275e
Serigne Saliou Mbacké NDIAYE <serignesalioumbackendiaye@esp.sn>
2023-01-28 20:08:54 +0000

    a

--- app/App.js
+++ app/App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="bg-cyan-600 justify-center items-center align-center">
+      <Text className='text-green-500'>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
